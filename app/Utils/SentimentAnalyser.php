c16a6d14cbddfd313503aa89321318d3ca81afec
Maroš Varchola <marosvarchola@sunray.sk>
2024-06-04 16:29:28 +0200

    feat: cleanup and added docs for another option to calculate sentiments locally

--- app/Utils/SentimentAnalyser.php
+++ app/Utils/SentimentAnalyser.php
@@ -2,6 +2,7 @@
 
 namespace SunrayEu\ProductDescriptionAnalyser\App\Utils;
 
+use Illuminate\Support\Facades\Log;
 use Google\Cloud\Language\LanguageClient;
 
 class SentimentAnalyser
@@ -9,18 +10,33 @@ class SentimentAnalyser
     private LanguageClient|null $gcpLangClient = null;
     private static SentimentAnalyser|null $instance = null;
 
+    // Here you set default value of processor, normally it is 'nlp.js', but it can be 'gcp' for google cloud platform
+    private static string $processor = 'nlp.js';
+
     /**
      * Load client if GCP environment variables are set.
      */
     public function __construct()
     {
-        $keyFilePath = $_ENV['GCP_LANG_CLIENT_SA_KEY_FILE_PATH'] ?? null;
-        if ((!empty($keyFilePath) && file_exists($keyFilePath)) || !empty($_ENV['GCP_LANG_CLIENT_SA_KEY'])) {
-            $this->gcpLangClient = new LanguageClient([
-                'keyFilePath' => $_ENV['GCP_LANG_CLIENT_SA_KEY_FILE_PATH'] ?? null,
-                'keyFile' => $_ENV['GCP_LANG_CLIENT_SA_KEY'] ?? null
-            ]);
+        if (!empty($_ENV['SENTIMENT_PROCESSOR_TYPE'])) {
+            $this->processor = $_ENV['SENTIMENT_PROCESSOR_TYPE'];
         }
+
+        switch ($this->processor) {
+            case 'gcp':
+                $keyFilePath = $_ENV['GCP_LANG_CLIENT_SA_KEY_FILE_PATH'] ?? null;
+                if ((!empty($keyFilePath) && file_exists($keyFilePath)) || !empty($_ENV['GCP_LANG_CLIENT_SA_KEY'])) {
+                    $this->gcpLangClient = new LanguageClient([
+                        'keyFilePath' => $_ENV['GCP_LANG_CLIENT_SA_KEY_FILE_PATH'] ?? null,
+                        'keyFile' => $_ENV['GCP_LANG_CLIENT_SA_KEY'] ?? null
+                    ]);
+                }
+                break;
+
+            default:
+                break;
+        }
+
     }
 
     public static function getInstance()
@@ -37,15 +53,23 @@ class SentimentAnalyser
         return self::getInstance()->gcpLangClient;
     }
 
-    public static function getSentimentScore(string $text): float
+    public static function getSentimentScore(string $text): float|null
     {
-        $gcpClient = self::getGcpClient();
+        switch (self::$processor) {
+            case 'gcp':
+                $gcpClient = self::getGcpClient();
+                if ($gcpClient) {
+                    $response = $gcpClient->analyzeSentiment($text);
+                    return $response->sentiment()['score'];
+                } else
+                    return null;
 
-        if ($gcpClient) {
-            $response = $gcpClient->analyzeSentiment($text);
-            return $response->sentiment()['score'];
-        } else {
-            return self::getLocalSentimentScore($text);
+            case 'nlp.js':
+                return self::getLocalSentimentScore($text);
+
+            default:
+
+                break;
         }
     }
 
@@ -55,11 +79,14 @@ class SentimentAnalyser
         $output = [];
         $returnVar = 0;
         exec("npm run analyse-sentiment $escapedText", $output, $returnVar);
+        $output = end($output);
+
+        Log::debug("Got output from npm analyse-sentiment: " . json_encode($output));
 
         if ($returnVar !== 0) {
             throw new \RuntimeException('Error running local sentiment analysis');
         }
 
-        return (float)trim($output[0]);
+        return (float) trim($output);
     }
 }
